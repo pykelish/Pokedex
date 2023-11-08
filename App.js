59072b75036a3f6af649d43e286f857f128c53d2
Emiliano Fernandez <emilianofh02@gmail.com>
2023-11-08 06:41:33 -0700

    pequeños cambios

--- App.js
+++ App.js
@@ -6,65 +6,77 @@ import Home from "./Views/Home";
 import Header from "./Template/Header";
 import { createContext, useEffect, useState } from "react";
 import Poke_Requests from "./models/Poke_Request";
+import { gql, useQuery } from "@apollo/client";
+import client from "./models/Client";
 
 const Stack = createNativeStackNavigator();
+const POKE_QUERY = gql`
+  query PokedexGQL {
+    pokemon_v2_pokemon {
+      name
+      id
+      pokemon_v2_pokemonsprites {
+        sprites
+      }
+      generation: pokemon_v2_pokemonspecy {
+        detail: pokemon_v2_generation {
+          name
+        }
+      }
+    }
+  }
+`;
 
 export default function App() {
   const [pokemonData, setPokemonData] = useState([]);
   const [groupedByGeneration, setGroupedByGeneration] = useState({});
-  const [loading, setLoading] = useState(true);
+  const { data, loading, error } = useQuery(POKE_QUERY);
+
   const DataContext = createContext();
 
   useEffect(() => {
     const getGeneration = async () => {
-      let pokemonDataAux = [];
-      const { results: generations } = await Poke_Requests.getAllGenerations();
-
-      // La generacion tiene que agregarse al objeto final
-      await Promise.all(
-        generations.map(async (generation, index) => {
-          const { pokemon_species } =
-            await Poke_Requests.getGenerationDetailsId(index + 1);
-          pokemonDataAux[generation.name] = [];
-
-          await Promise.all(
-            pokemon_species.map(async (pokemon_specie) => {
-              const { varieties } = await Poke_Requests.getPokemonSpecieDetail(
-                pokemon_specie.name
-              );
-
-              await Promise.all(
-                varieties.map(async ({ pokemon }) => {
-                  const request = await Poke_Requests.getPokemon(pokemon.name);
-                  request["generation"] = generation.name;
-
-                  if (pokemonDataAux.length < 50) {
-                    pokemonDataAux.push(request);
-                    if (pokemonDataAux.length < 50) return;
-                  }
-
-                  setPokemonData((prevPokemonData) => [
-                    ...prevPokemonData,
-                    ...pokemonDataAux,
-                  ]);
-                  pokemonDataAux = [];
-                })
-              );
-            })
-          );
-        })
-      );
-
-      if (pokemonDataAux.length > 0) {
-        setPokemonData((prevPokemonData) => [
-          ...prevPokemonData,
-          ...pokemonDataAux,
-        ]);
-
-        setLoading(false);
-      }
-
-      // groupByGeneration();
+      console.log(data);
+      // let pokemonDataAux = [];
+      // const { results: generations } = await Poke_Requests.getAllGenerations();
+      // // La generacion tiene que agregarse al objeto final
+      // await Promise.all(
+      //   generations.map(async (generation, index) => {
+      //     const { pokemon_species } =
+      //       await Poke_Requests.getGenerationDetailsId(index + 1);
+      //     pokemonDataAux[generation.name] = [];
+      //     await Promise.all(
+      //       pokemon_species.map(async (pokemon_specie) => {
+      //         const { varieties } = await Poke_Requests.getPokemonSpecieDetail(
+      //           pokemon_specie.name
+      //         );
+      //         await Promise.all(
+      //           varieties.map(async ({ pokemon }) => {
+      //             const request = await Poke_Requests.getPokemon(pokemon.name);
+      //             request["generation"] = generation.name;
+      //             if (pokemonDataAux.length < 50) {
+      //               pokemonDataAux.push(request);
+      //               if (pokemonDataAux.length < 50) return;
+      //             }
+      //             setPokemonData((prevPokemonData) => [
+      //               ...prevPokemonData,
+      //               ...pokemonDataAux,
+      //             ]);
+      //             pokemonDataAux = [];
+      //           })
+      //         );
+      //       })
+      //     );
+      //   })
+      // );
+      // if (pokemonDataAux.length > 0) {
+      //   setPokemonData((prevPokemonData) => [
+      //     ...prevPokemonData,
+      //     ...pokemonDataAux,
+      //   ]);
+      //   setLoading(false);
+      // }
+      // // groupByGeneration();
     };
 
     // getPokemonData();
@@ -92,18 +104,23 @@ export default function App() {
   //   // groupByGeneration();
   // }, [pokemonData]);
 
-  if(loading) {
+  if (loading) {
     return (
-      <View style={{display: "flex", height: '100%', justifyContent: "center", alignItems: "center"}}>
+      <View
+        style={{
+          display: "flex",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Text>Loading {(pokemonData.length / 1292).toFixed(2) * 100}%</Text>
       </View>
-    )
-  } 
+    );
+  }
   return (
-    <DataContext.Provider value={pokemonData}>
-        <Text>{pokemonData.length}</Text>
+    <ApolloProvider client={client}>
       <NavigationContainer>
-        {/* <SafeAreaView style={{ flex: 1 }}> */}
         <Stack.Navigator
           initialRouteName="home"
           screenOptions={{
@@ -115,11 +132,13 @@ export default function App() {
             },
           }}
         >
-          <Stack.Screen name="home" component={Home}/>
+          <Stack.Screen name="home" component={Home} />
         </Stack.Navigator>
+
+        {/* <SafeAreaView style={{ flex: 1 }}> */}
         {/* </SafeAreaView> */}
       </NavigationContainer>
-    </DataContext.Provider>
+    </ApolloProvider>
   );
 }
 
